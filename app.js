ef28d4c26463d97d9b7f8c3c4a8b2415761a7e7a
sarah23398 <sarah23398@yahoo.ca>
2021-12-07 21:32:37 -0500

    Added login route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const sassMiddleware = require('node-sass-middleware');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const registerRouter = require('./routes/register');
+const loginRouter = require('./routes/login')
 
 const app = express();
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/register', registerRouter);
+app.use('/login', loginRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
