9f82886cb03b530e01315cb664c5f553c4b79620
AdiChops <aaditya.chopra@carleton.ca>
2021-12-09 22:27:26 -0500

    owner auth

--- routes/login.js
+++ routes/login.js
@@ -1,20 +1,19 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+const OWNER_EMAIL = 'owner@nook.ca';
+const OWNER_PASSWORD = 'youshallnotpass';
 
 /* GET login page. */
 router.get('/', function(req, res, next) {
     res.render('login', { title: 'Login' });
   });
 
-router.post('/', function(req, res, next) {
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
-    req.body = {
-      'email': email,
-      'password': password
+router.post('/owner', function(req, res, next) {
+    if(req.body.email === OWNER_EMAIL && req.body.password === OWNER_PASSWORD){
+      res.status(200).redirect('/');
     }
-    if (res.status === 200) {
-      console.log('nice');
+    else{
+      res.status(401).send('Incorrect email and/or password');
     }
   });
 module.exports = router;
