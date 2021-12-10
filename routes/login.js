2aced8a39e11afe8523cfa8b442a55ea4210e740
sarah23398 <sarah23398@yahoo.ca>
2021-12-10 00:24:17 -0500

    Clean up

--- routes/login.js
+++ routes/login.js
@@ -9,13 +9,10 @@ router.get('/', function(req, res, next) {
   });
 
 router.post('/owner', function(req, res, next) {
-  console.log(req.body);
     if(req.body.email === OWNER_EMAIL && req.body.password === OWNER_PASSWORD){
-      res.status(200);
+      res.status(200).send();
     }
     else{
-      console.log(req.body.email)
-      console.log(req.body.password)
       res.status(401).send('Incorrect email and/or password');
     }
   });
