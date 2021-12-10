17c0c3c07f54485653ea0f320d583a82bd766ee9
sarah23398 <sarah23398@yahoo.ca>
2021-12-09 22:19:09 -0500

    Login method

--- routes/login.js
+++ routes/login.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
     res.render('login', { title: 'Login' });
   });
 
+router.post('/auth', function(req, res, next) {
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    req.body = {
+      'email': email,
+      'password': password
+    }
+    if (res.status === 200) {
+      console.log('nice');
+    }
+  });
 module.exports = router;
