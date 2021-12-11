dd85597ccda0d2fb7f6045dd47ff1b340573bd30
AdiChops <aaditya.chopra@carleton.ca>
2021-12-11 03:08:51 -0500

    order insert

---
+++ sql/insert_orders.sql
@@ -0,0 +1,140 @@
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('1', '18', '2016-12-28', '4141647344128244', '2630 Tabitha Square Williamshaven, NS X9B 1G8', '2630 Tabitha Square Williamshaven, NS X9B 1G8', 'FULFILLED');
+    
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('2', '6', '2021-07-07', '4297921291037761', '975 Harvey Flats Suite 389 Millerland, MB M9M3R2', '975 Harvey Flats Suite 389 Millerland, MB M9M3R2', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('3', '14', '2018-12-07', '4962872377732466', '598 James Knoll Apt. 844 South Robert, ON M9B 6R2', '5376 Powell Landing South Sarah, NB E8N 7N2', 'FULFILLED');
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('4', '4', '2020-12-05', '4299187313111061', '0182 Raymond Wall Amyborough, AB G6R1A9', '0182 Raymond Wall Amyborough, AB G6R1A9', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('5', '21', '2015-12-14', '4280825959442107', '368 Matthew Valley New Katie, YT H7A5T2', '368 Matthew Valley New Katie, YT H7A5T2', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('6', '13', '2021-05-04', '4367417940617835', '5423 Angelica Flats Tapialand, BC J2K 1T4', '4821 Brady Canyon North Thomaschester, SK K4M3J4', 'FULFILLED');   
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('7', '12', '2015-12-02', '4712428314751581', '6243 Angela Glens Suite 928 North Evan, AB E4E8C8', '6243 Angela Glens Suite 928 North Evan, AB E4E8C8', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('8', '12', '2015-06-30', '4924975340458676', '44743 Michele Springs Apt. 615 Lake Nathaniel, QC Y1G 5C7', '44743 Michele Springs Apt. 615 Lake Nathaniel, QC Y1G 5C7', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('9', '2', '2017-08-29', '4021489516056654', '389 Wilson Camp Apt. 514 East Ryan, PE P4A2B7', '11860 Smith Island Apt. 714 North Carla, YT L5X 1P1', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('10', '2', '2018-03-26', '4184654470788740', '206 Andrew Lock South Priscilla, NU L4T4A1', '206 Andrew Lock South Priscilla, NU L4T4A1', 'FULFILLED');        
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('11', '10', '2018-07-18', '4226549646310249', '90743 Russo Square New Karenview, NU C6T 5V5', '90743 Russo Square New Karenview, NU C6T 5V5', 'FULFILLED');   
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('12', '6', '2019-02-17', '4433818207553003', '801 Michael Point Apt. 116 Ellismouth, AB A8A 2S6', '801 Michael Point Apt. 116 Ellismouth, AB A8A 2S6', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('13', '2', '2018-03-16', '4953500475644132', '8056 Vasquez Harbor Suite 039 West Kathryn, YT V8K2C8', '8056 Vasquez Harbor Suite 039 West Kathryn, YT V8K2C8', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('14', '19', '2019-01-16', '4414470618314766', '703 Wright Spring Sawyerton, NS A3Y9A4', '703 Wright Spring Sawyerton, NS A3Y9A4', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('15', '18', '2016-01-13', '4776863567984849', '927 Flowers Lodge Suite 933 West Matthew, BC X8Y5H2', '927 Flowers Lodge Suite 933 West Matthew, BC X8Y5H2', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('16', '1', '2017-08-10', '4977871568703651', '13886 Patrick Valleys Apt. 321 Wrightmouth, PE Y4X3L1', '13886 Patrick Valleys Apt. 321 Wrightmouth, PE Y4X3L1', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('17', '6', '2019-07-14', '4354479654157544', '984 Goodwin Forks Suite 469 Hamiltonside, PE E8C2V9', '984 Goodwin Forks Suite 469 Hamiltonside, PE E8C2V9', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('18', '19', '2016-07-27', '4105040437233051', '07268 Elizabeth Rest Suite 764 West Jacob, NU C7G 7X5', '07268 Elizabeth Rest Suite 764 West Jacob, NU C7G 7X5', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('19', '12', '2020-06-03', '4245527479938812', '240 Phillips Plaza Davidton, ON S9G 8R5', '240 Phillips Plaza Davidton, ON S9G 8R5', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('20', '19', '2017-02-13', '4616015771378433', '792 Harrison Pines Suite 952 Lake Elizabeth, AB Y7B 8G3', '76673 Lawrence Forge Juliefort, BC N2B 9E7', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('21', '11', '2021-08-14', '4172189925660545', '462 Berger Canyon Apt. 615 Jasontown, BC R7B1C3', '815 Lucas Street Jimenezport, QC B5A8B5', 'FULFILLED');     
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('22', '16', '2021-01-18', '4422811672346949', '722 Underwood Pine Apt. 767 West Stephanie, PE K6K5V7', '2262 Stephen Junction Michaeltown, PE G6J 8E6', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('23', '19', '2016-05-20', '4936672811049508', '05422 Murray Wells Apt. 222 Annaport, NU J7J 3J2', '400 Arnold Gardens East Diana, QC S2C 9S7', 'FULFILLED');  
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('24', '8', '2020-01-07', '4291962217074211', '72315 Obrien Garden Suite 817 Michelemouth, PE B2R 3C3', '72315 Obrien Garden Suite 817 Michelemouth, PE B2R 3C3', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('25', '20', '2020-10-31', '4217360867120825', '7276 Olivia Drives Coreyside, MB V8E 9J1', '215 Smith Center Lindseyhaven, ON H8E8Y9', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('26', '21', '2021-03-13', '4131087834121510', '01032 Jackson Harbors Suite 818 Scottbury, NB H1G 2T5', '5676 Young Pike Port Davidmouth, ON Y1B5R7', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('27', '5', '2019-02-10', '4374611601035714', '14154 Edward Mill North Kenneth, NL P7J9A6', '14154 Edward Mill North Kenneth, NL P7J9A6', 'FULFILLED');        
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('28', '18', '2018-03-01', '4651155127834436', '34334 Jones Estate Suite 764 Gallaghershire, ON E9A 7A6', '34334 Jones Estate Suite 764 Gallaghershire, ON E9A 
+7A6', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('29', '21', '2017-07-11', '4640174913358793', '1484 Krystal Stravenue Apt. 534 Troymouth, NT H4K5C5', '1484 Krystal Stravenue Apt. 534 Troymouth, NT H4K5C5', 
+'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('30', '1', '2018-11-04', '4242961901705171', '56009 Walker Orchard Suite 579 West Samanthabury, QC N6R2B5', '56009 Walker Orchard Suite 579 West Samanthabury, QC N6R2B5', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('31', '7', '2020-01-04', '4305119472539081', '91943 Aguilar Alley Apt. 136 Staceyville, PE T1X 1V6', '99351 Courtney Crossroad Apt. 070 Port Kathleen, NS C9H4X6', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('32', '17', '2020-05-22', '4484959678255534', '36102 Crystal Ridge Apt. 162 Toddhaven, QC L7A 4M2', '36102 Crystal Ridge Apt. 162 Toddhaven, QC L7A 4M2', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('33', '20', '2021-10-25', '4597648545834068', '698 Orr Corner Danielhaven, NT H5A 4E3', '816 Coleman Alley Suite 913 North Adam, NB X2A 2J5', 'FULFILLED');   
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('34', '5', '2019-08-14', '4475646538487099', '616 Carroll Well Suite 969 South Taylor, SK T1B2S2', '616 Carroll Well Suite 969 South Taylor, SK T1B2S2', 'FULFILLED');
+
+INSERT INTO public."order"(
+            id, customer_id, order_date, bank_account, billing_address, destination, status)
+            VALUES ('35', '20', '2018-09-24', '4972375375257146', '26929 Allen Field Port Brandymouth, QC L6V 1H2', '26929 Allen Field Port Brandymouth, QC L6V 1H2', 'FULFILLED');
--- sqlorders.py
+++ sqlorders.py
@@ -1,14 +1,19 @@
 from faker import Faker
 from datetime import datetime
 
-fake = Faker()
+fake = Faker('en_CA')
 
 for i in range(0, 35):
     custid = fake.pyint(1, 21)
     orderdate = fake.date_between(datetime(2015,5,29))
     bankacc = fake.credit_card_number('visa16')
+    billing = fake.address().replace("\n"," ")
+    if fake.pyint(0, 1) == 0:
+        dest = fake.address().replace("\n"," ")
+    else:
+        dest = billing
 
-print(f'''INSERT INTO public."order"(
-	id, customer_id, order_date, bank_account, billing_address, destination, status)
-	VALUES (?, ?, ?, ?, ?, ?, ?);
-''')
+    print(f'''INSERT INTO public."order"(
+	    id, customer_id, order_date, bank_account, billing_address, destination, status)
+	    VALUES ('{i+1}', '{custid}', '{orderdate}', '{bankacc}', '{billing}', '{dest}', 'FULFILLED');
+    ''')
