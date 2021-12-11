204ff683b9fb4f10843bfa35206127c3e16a7d6b
AdiChops <aaditya.chopra@carleton.ca>
2021-12-11 02:31:22 -0500

    script generator for customers and insert cust

--- sql.py
+++ sql.py
@@ -1,2 +1,14 @@
-import faker
+from faker import Faker
 
+fake = Faker('en_CA')
+
+for i in range(0, 21):
+    name = fake.name()
+    email = fake.email()
+    address = fake.address().replace("\n"," ")
+    phone = fake.msisdn()[3:]
+    bankacc = fake.credit_card_number('visa16')
+    password = email[0:email.find("@")].lower()
+    print(f'''INSERT INTO public.customer(
+	    id, name, email, password, phone, address, bank_account)
+	    VALUES ('{i+1}','{name}', '{email}', '{password}', '{phone}', '{address}', '{bankacc}');''')
