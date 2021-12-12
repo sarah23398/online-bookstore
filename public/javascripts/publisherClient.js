47e1694796026bc654fa8a004b9bb6c7b337a930
Sarah <32947001+sarah23398@users.noreply.github.com>
2021-12-11 23:09:05 -0500

    Sarah/add publisher (#5)
    * Create publisher client script
    
    * Create publisher handler
    
    * Publisher created

---
+++ public/javascripts/publisherClient.js
@@ -0,0 +1,34 @@
+function createPublisher(e) {
+    e.preventDefault();
+      const name = document.getElementById('publisherName').value;
+      const email = document.getElementById('publisherEmail').value;
+      const phone = document.getElementById('publisherPhone').value;
+      const address = document.getElementById('publisherAddress').value;
+      const bankAccount = document.getElementById('publisherBankAccount').value;
+      fetch('/publishers/add', {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+              'name': name,
+              'email': email,
+              'phone': phone, 
+              'address': address,
+              'bankAccount': bankAccount
+          })
+        })
+        .then((response) => {
+          if(response.status == 201){
+            alert('Publisher successfully added.');
+          }
+          else{
+            alert('Publisher could not be added.');
+          }
+        })
+        .catch((err)=>{
+          console.error(err)
+        })
+  };
+  
+  document.getElementById('addPublisher').addEventListener('click', createPublisher);
--- routes/publishers.js
+++ routes/publishers.js
@@ -11,4 +11,16 @@ router.get('/add', function(req, res, next){
   res.render('add-publisher');
 })
 
+router.post('/add', function(req, res, next) {
+  const id = '5';
+  req.app.locals.client.query('INSERT INTO publisher(id, name, email, phone, address, bank_account) VALUES ($1, $2, $3, $4, $5, $6);', 
+    [id, req.body.name, req.body.email, req.body.phone, req.body.address, req.body.bankAccount], (error, results) => {
+    if (error) {
+      console.log(error);
+      res.status(500).json({success: false, data: err});
+    }
+    res.status(201).send(`Publisher added with ID: ${results.insertId}`)
+  })
+})
+
 module.exports = router;
