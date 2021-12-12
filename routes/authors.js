f61938e60d3e277c1a04e6c2ed393b08a3dc09c8
sarah23398 <sarah23398@yahoo.ca>
2021-12-11 22:45:27 -0500

    Author created

--- routes/authors.js
+++ routes/authors.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const { response } = require('../app');
 var router = express.Router();
 
 router.get('/add', function(req, res, next){
@@ -6,4 +7,15 @@ router.get('/add', function(req, res, next){
   res.render('add-author');
 })
 
+router.post('/add', function(req, res, next) {
+  const id = '6';
+  req.app.locals.client.query('INSERT INTO author(id, name, email, bio, website) VALUES ($1, $2, $3, $4, $5);', 
+    [id, req.body.name, req.body.email, req.body.bio, req.body.website], (error, results) => {
+    if (error) {
+      console.log(err);
+      res.status(500).json({success: false, data: err});
+    }
+    res.status(201).send(`Author added with ID: ${results.insertId}`)
+  })
+})
 module.exports = router;
