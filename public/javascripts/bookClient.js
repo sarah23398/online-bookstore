45f7d43156cd4c5d2e48ad8e572be684f8a50a81
Sarah <32947001+sarah23398@users.noreply.github.com>
2021-12-14 00:23:30 -0500

    Sarah/remove book (#7)
    * Remove book handler
    
    * Delete method
    
    * Adding isbn
    
    * Fixing error handlers
    
    * Remove book handler
    
    * Delete method
    
    * Adding isbn
    
    * Fixing error handlers

--- public/javascripts/bookClient.js
+++ public/javascripts/bookClient.js
@@ -1,48 +1,24 @@
-function createBook(e) {
-  e.preventDefault();
-    const title = document.getElementById('bookTitle').value;
-    const author = document.getElementById('bookAuthor').value;
-    const isbn = document.getElementById('bookIsbn').value;
-    const genre = document.getElementById('bookGenre').value;
-    const price = document.getElementById('bookPrice').value;
-    const publisher = document.getElementById('bookPublisher').value;
-    const publishDate = document.getElementById('bookPublishDate').value;
-    const edition = document.getElementById('bookEdition').value;
-    const description = document.getElementById('bookDescription').value;
-    const printLength = document.getElementById('bookPages').value;
-    const stock = document.getElementById('bookStock').value;
-    const publisherFee = document.getElementById('bookPublisherFee').value;
-    fetch('/books/add', {
-        method: 'POST',
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            'title': title, 
-            'author': author, 
-            'isbn': isbn, 
-            'genre': genre, 
-            'price': price,
-            'publisher': publisher,
-            'publishDate': publishDate,
-            'edition': edition, 
-            'description': description, 
-            'printLength': printLength,
-            'stock': stock,
-            'publisherFee': publisherFee
+function deleteBook(e) {
+    const isbn = document.getElementById('isbn').innerHTML;
+    e.preventDefault();
+      fetch('/books/' + isbn, {
+          method: 'DELETE',
+          headers: {
+            "Content-Type": "application/json"
+          }
         })
-      })
-      .then((response) => {
-        if(response.status == 201){
-          alert('Book added successfully.');
-        }
-        else{
-          alert('Book could not be added.');
-        }
-      })
-      .catch((err)=>{
-        console.error(err)
-      })
-};
-
-document.getElementById('addBook').addEventListener('click', createBook)
+        .then((response) => {
+          if(response.status == 204){
+            alert('Book removed successfully.');
+            location.href = '/books';
+          }
+          else{
+            alert('Book could not be removed.');
+          }
+        })
+        .catch((err)=>{
+          console.error(err)
+        })
+  };
+  
+  document.getElementById('removeBook').addEventListener('click', deleteBook)
