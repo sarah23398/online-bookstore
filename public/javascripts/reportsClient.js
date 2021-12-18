dc478f3bf4370d587bedbd18c3475173ed1d9c6b
AdiChops <aaditya.chopra@carleton.ca>
2021-12-17 23:48:38 -0500

    custom reports are done 🥳

--- public/javascripts/reportsClient.js
+++ public/javascripts/reportsClient.js
@@ -4,65 +4,11 @@ function getCustomReport(e) {
     const start = document.getElementById('customReportStartDate').value;
     const end = document.getElementById('customReportEndDate').value;
     if (type == 'author') {
-        fetch('/reports/custom/author', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "text/html"
-            },
-            body: JSON.stringify({
-                'start': start,
-                'end': end
-            })
-        })
-        .then((response) => {
-            if(response.status != 200) {
-                alert("Invalid dates.");
-            }
-        })
-        .catch((err) => {
-            console.error(err);
-        });
+        location.href = `/reports/author?start=${start}&end=${end}`;
     } else if (type == 'expenditure') {
-        fetch('/reports/custom/expenditures', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "text/html"
-            },
-            body: JSON.stringify({
-                'start': start,
-                'end': end
-            })
-        })
-        .then((response) => {
-            if(response.status != 200) {
-                alert("Invalid dates.");
-            }
-        })
-        .catch((err) => {
-            console.error(err);
-        });
+        location.href = `/reports/expenditures?start=${start}&end=${end}`;
     } else {
-        fetch('/reports/custom/genre', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "text/html"
-            },
-            body: JSON.stringify({
-                'start': start,
-                'end': end
-            })
-        })
-        .then((response) => {
-            if(response.status != 200) {
-                alert("Invalid dates.");
-            }
-        })
-        .catch((err) => {
-            console.error(err);
-        });
+        location.href = `/reports/genre?start=${start}&end=${end}`;
     }
 }
 
--- routes/reports.js
+++ routes/reports.js
@@ -7,61 +7,44 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/author', function(req, res, next){ 
-  req.app.locals.client.query(`SELECT author_id, SUM(price*quantity)
+  let extra = 'WHERE';
+  let title = 'Sales per Author Report';
+  params = [];
+  if(req.query.start && req.query.end){
+    extra = `INNER JOIN "order" on finances.order_id = "order".id
+    WHERE "order".order_date BETWEEN $1 AND $2 AND`;
+    params = [req.query.start, req.query.end];
+    title += ` (custom from ${req.query.start} to ${req.query.end})`
+  }
+  req.app.locals.client.query(`SELECT author.name, SUM(book.price*finances.quantity)
   FROM written_by
+  INNER JOIN author ON written_by.author_id = author.id
   INNER JOIN book ON book.ISBN = written_by.ISBN
   INNER JOIN finances on finances.ISBN = written_by.ISBN
-  WHERE book.ISBN = written_by.ISBN
-  GROUP BY author_id`, [], (err, result)=>{
-    let report = [];
-    for (let author of result.rows){
-      report.push([author.author_id, `$${author.sum}`])
-    }
-    console.log(report)
-    res.render('report', {report: report, report_title: 'Sales per Author Report', headers: ['Author', 'Sales'] })
-  })
-});
-
-router.post('/custom/author', function(req, res, next){ 
-  req.app.locals.client.query(`SELECT author_id, SUM(price*quantity)
-  FROM written_by
-  INNER JOIN book ON book.ISBN = written_by.ISBN
-  INNER JOIN finances on finances.ISBN = written_by.ISBN
-  INNER JOIN "order" on finances.order_id = "order".id
-  WHERE book.ISBN = written_by.ISBN
-  AND "order".order_date BETWEEN $1 AND $2
-  GROUP BY author_id;`, [req.body.start, req.body.end], (err, result)=>{
+  ${extra}
+  book.ISBN = written_by.ISBN
+  GROUP BY author.name;`, params, (err, result)=>{
     let report = [];
     console.log(req.body, result)
     for (let author of result.rows){
-      report.push([author.author_id, `$${author.sum}`])
+      report.push([author.name, `$${author.sum}`])
     }
     console.log(report);
-    res.render('report', {report: report, report_title: 'Sales per Author Report', headers: ['Author', 'Sales'] })
+    res.render('report', {report: report, report_title: title, headers: ['Author', 'Sales'] })
   })
 });
+
 
 router.get('/expenditures', function(req, res, next){
-  req.app.locals.client.query(`SELECT publisher_name as publisher, SUM(publisher_earnings) as publisher_earnings, SUM(sales) as sales ,SUM(revenue) as net_revenue FROM (SELECT 
-    publisher.name as publisher_name, 
-    ROUND((book.publisher_fee*(SUM(book.price*finances.quantity))/100),2) AS publisher_earnings, 
-    SUM(book.price*finances.quantity) AS sales,
-    SUM(book.price*finances.quantity)-(ROUND((book.publisher_fee*(SUM(book.price*finances.quantity))/100),2)) AS revenue
-    FROM publisher
-    INNER JOIN book on publisher.id = book.publisher_id
-    INNER JOIN finances on finances.ISBN = book.ISBN 
-    GROUP BY publisher.name, 
-    book.publisher_fee) as earnings_per_book group by publisher_name;`, [], (err, result)=>{
-    let report = [];
-    for (let res of result.rows){
-      report.push([res.publisher, `$${res.sales}`, `$${res.publisher_earnings}`, `$${res.net_revenue}`])
-    }
-    console.log(report)
-    res.render('report', {report: report, report_title: 'Sales vs. Expenditures Report', headers: ['Publisher', 'Total Sales', 'Total Publisher Earnings (paid to publisher)', 'Net Revenue'] })
-  })
-});
-
-router.post('/custom/expenditures', function(req, res, next){
+  let extra = '';
+  let title = 'Sales vs. Expenditures Report';
+  params = [];
+  if(req.query.start && req.query.end){
+    extra = `INNER JOIN "order" on finances.order_id = "order".id
+    WHERE "order".order_date BETWEEN $1 AND $2`;
+    params = [req.query.start, req.query.end];
+    title += ` (custom from ${req.query.start} to ${req.query.end})`;
+  }
   req.app.locals.client.query(`SELECT publisher_name as publisher, SUM(publisher_earnings) as publisher_earnings, SUM(sales) as sales ,SUM(revenue) as net_revenue FROM (SELECT 
     publisher.name as publisher_name, 
     ROUND((book.publisher_fee*(SUM(book.price*finances.quantity))/100),2) AS publisher_earnings, 
@@ -70,54 +53,45 @@ router.post('/custom/expenditures', function(req, res, next){
     FROM publisher
     INNER JOIN book on publisher.id = book.publisher_id
     INNER JOIN finances on finances.ISBN = book.ISBN
-	  INNER JOIN "order" on finances.order_id = "order".id
-    WHERE "order".order_date BETWEEN $1 AND $2
+    ${extra}
     GROUP BY publisher.name, 
-    book.publisher_fee) as earnings_per_book group by publisher_name;`, [req.body.start, req.body.end], (err, result)=>{
+    book.publisher_fee) as earnings_per_book group by publisher_name;`, [req.query.start, req.query.end], (err, result)=>{
     let report = [];
     for (let res of result.rows){
       report.push([res.publisher, `$${res.sales}`, `$${res.publisher_earnings}`, `$${res.net_revenue}`])
     }
     console.log(report)
-    res.render('report', {report: report, report_title: 'Sales vs. Expenditures Report', headers: ['Publisher', 'Total Sales', 'Total Publisher Earnings (paid to publisher)', 'Net Revenue'] })
+    res.render('report', {report: report, report_title: title, headers: ['Publisher', 'Total Sales', 'Total Publisher Earnings (paid to publisher)', 'Net Revenue'] })
   })
 });
 
 router.get('/genre', function(req, res, next){
+  let extra = '';
+  let title = 'Sales per Genre Report';
+  params = [];
+  if(req.query.start && req.query.end){
+    extra = `INNER JOIN "order" on finances.order_id = "order".id
+    WHERE "order".order_date BETWEEN $1 AND $2`;
+    params = [req.query.start, req.query.end];
+    title += ` (custom from ${req.query.start} to ${req.query.end})`;
+  }
   req.app.locals.client.query(`SELECT genre.name, SUM(book.price*finances.quantity)
   FROM genre
   INNER JOIN "contains" ON "contains".genre_id = genre.id
   INNER JOIN book on "contains".ISBN = book.ISBN
   INNER JOIN finances ON finances.ISBN = book.ISBN
-  GROUP BY genre.name`, [], (err, result)=>{
+  ${extra}
+  GROUP BY genre.name`, params, (err, result)=>{
     let report = [];
 
     for (let genre of result.rows){
       report.push([genre.name, `$${genre.sum}`])
     }
     console.log(report)
-    res.render('report', {report: report, report_title: 'Sales per Genre Report', headers: ['Genre', 'Sales'] })
+    res.render('report', {report: report, report_title: title, headers: ['Genre', 'Sales'] })
   })
 });
 
-router.post('/custom/genre', function(req, res, next){
-  req.app.locals.client.query(`SELECT genre.name, SUM(book.price*finances.quantity)
-  FROM genre
-  INNER JOIN "contains" ON "contains".genre_id = genre.id
-  INNER JOIN book on "contains".ISBN = book.ISBN
-  INNER JOIN finances ON finances.ISBN = book.ISBN
-  INNER JOIN "order" on finances.order_id = "order".id
-  WHERE "order".order_date BETWEEN $1 AND $2
-  GROUP BY genre.name;`, [req.body.start, req.body.end], (err, result)=>{
-    let report = [];
-
-    for (let genre of result.rows){
-      report.push([genre.name, `$${genre.sum}`])
-    }
-    console.log(report)
-    res.render('report', {report: report, report_title: 'Sales per Genre Report', headers: ['Genre', 'Sales'] })
-  })
-});
 
 module.exports = router;
 
