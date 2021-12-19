8277c3f0894abb39ca06abe788bbc506f5d8f4b0
AdiChops <aaditya.chopra@carleton.ca>
2021-12-18 22:49:31 -0500

    push mock data

--- sql/ddl-dml/all_inserts.sql
+++ sql/ddl-dml/all_inserts.sql
@@ -1163,3 +1163,216 @@ INSERT INTO public.finances(
 INSERT INTO public.finances(
                 order_id, isbn, quantity)
                 VALUES ('35', '1928374858349', 1);
+
+--- RATINGS
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (15, '9842038473847', 3, 'No improvement flawless alright bad read. Well good improvement greatly real awful.
+Worldwide watch no yes rewatch real. Good fiction greatly book watch.
+Had greatly really worldwide.
+Again comic rewatch rewind worldwide rewind yes bad.', '2017-08-25');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (15, '1928374858349', 1, 'Greatly good improvement improvement yes worldwide improvement scenes. Again again comic read good worldwide.
+Rewatch awful no watch worldwide read yes. Great fiction watch really.', '2016-06-18');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (5, '1928374858349', 3, 'Had fiction yes done rewind. Had no great again.
+Comic flawless done really movie great comic. Amazing improvement read real. Had movie had tv show great worldwide watch.', '2021-12-10');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (12, '2738490598231', 3, 'Great read movie watch rewatch alright. No worldwide read tv show movie. Great improvement movie.
+Rewatch watch book bad had rewind bad. Had amazing book well greatly had movie.', '2021-09-23');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (21, '9283745689127', 1, 'Movie rewatch done really rewatch flawless. Tv Show done yes watch. Yes tv show rewatch well.
+Flawless scenes alright.', '2019-08-07');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (7, '9842038473847', 3, 'Greatly improvement no read. Really flawless improvement really good again. Awful greatly read no. Real done rewind.      
+Good watch flawless good movie.
+Rewatch flawless no real. Done great tv show well amazing. Fiction awful tv show done.', '2015-07-13');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (2, '1928374858349', 5, 'Really rewind awful bad had. Rewatch alright rewind great.
+Read bad done fiction great. Movie well alright watch rewind greatly.', '2016-07-22');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (12, '2738490598231', 2, 'No book had comic watch. Improvement well great yes greatly good.
+Greatly bad good well book fiction bad. Worldwide well done again. Scenes really read book.
+Greatly had no again. Yes comic improvement greatly really greatly done.', '2017-01-13');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (19, '2738490598231', 4, 'Done watch flawless awful amazing read. Yes scenes again done. Good really bad yes worldwide awful alright.
+Fiction real alright worldwide greatly watch book.
+Movie greatly rewind. Improvement had worldwide amazing tv show.', '2021-06-14');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (4, '2738490598231', 3, 'Flawless done greatly. Fiction worldwide read book. Had again no well.
+Had yes movie rewind improvement. Amazing watch scenes worldwide tv show.', '2016-09-10');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (1, '9842038473847', 5, 'Fiction flawless good watch comic. Read yes good movie worldwide.
+Scenes great alright improvement rewatch good. Rewatch great yes flawless awful worldwide rewind amazing.
+Good worldwide yes real flawless. Well bad greatly fiction.', '2019-03-31');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (8, '1234567890123', 5, 'Flawless good greatly. Good yes well improvement tv show watch improvement. Scenes good improvement.
+Scenes improvement real watch great tv show. Improvement worldwide amazing well really. Again done real.', '2015-07-04');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (19, '2738490598231', 4, 'Tv Show watch no awful awful comic rewind. Real read movie amazing improvement well tv show rewind. Comic no bad rewatch 
+worldwide scenes no.
+Greatly bad alright comic yes read tv show. Book real comic had.', '2021-11-18');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (1, '1928374858349', 1, 'Really read read fiction comic fiction. Yes really watch bad. Bad rewind awful flawless really movie worldwide.
+Tv Show book scenes rewatch. Rewatch awful well good book.', '2019-04-23');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (9, '3748596758431', 3, 'Alright movie fiction real watch rewind no amazing.
+Book tv show greatly rewind had good comic worldwide. Movie scenes real watch tv show great.
+Awful alright read. Movie yes done fiction had.', '2020-10-02');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (20, '9283745689127', 1, 'Read real scenes real. Flawless great great.
+Real rewind really. Watch tv show movie rewatch improvement. Book amazing read alright book alright.
+No really fiction well. Watch no no improvement rewatch read fiction.', '2019-10-17');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (8, '9842038473847', 5, 'Greatly bad comic yes movie scenes. Great rewind great worldwide again done yes had.
+No flawless awful comic comic well great. Comic good alright well had alright. Done bad movie greatly.', '2018-07-22');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (2, '2938475647361', 4, 'Again greatly well no. Flawless rewatch good tv show fiction rewind watch great. Real good flawless great good fiction done.', '2019-09-04');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (19, '2738490598231', 1, 'Bad bad comic worldwide read greatly great. Had well flawless greatly tv show good scenes. Flawless no well great watch great scenes. Yes real great had.', '2020-09-08');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (7, '1928374858349', 5, 'Worldwide flawless rewind awful. Real fiction no movie. Great worldwide tv show book no movie.
+Awful improvement well had real worldwide.', '2020-11-11');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (17, '3748596758431', 2, 'Rewatch again amazing tv show yes no watch. Watch greatly good awful bad flawless rewatch.
+Amazing had improvement really good. Greatly improvement again movie comic.
+Improvement again scenes flawless good. Comic had awful.', '2017-11-22');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (11, '2738490598231', 2, 'Had greatly done well. Well scenes had bad again movie improvement. Bad improvement comic tv show again.
+Real bad amazing amazing read great awful. Amazing rewind amazing movie. Read flawless again fiction fiction watch.', '2016-04-07');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (5, '3748596758431', 5, 'Amazing had really rewind really amazing real improvement. Well rewatch alright comic well. Watch yes worldwide.', '2019-07-07');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (15, '9283745689127', 2, 'Rewatch bad watch improvement tv show improvement. Movie awful again alright scenes great awful. Fiction tv show no yes rewind.
+Well flawless again really movie amazing done done. Good great bad worldwide.', '2019-10-17');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (2, '1928374858349', 1, 'Worldwide tv show read amazing again done. Well bad read awful bad greatly book. Yes awful had comic rewatch great done.  
+Bad rewatch really yes. Done rewatch yes amazing.
+Fiction had great watch comic. Worldwide no rewind scenes.', '2019-05-16');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (15, '2938475647361', 4, 'Rewatch again rewatch done. Really again no greatly. Alright rewatch good fiction. Book book scenes watch movie awful no.Flawless fiction worldwide had no. Good greatly improvement tv show. Awful bad done really tv show done flawless yes.', '2017-02-11');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (2, '9283745689127', 5, 'Scenes awful well alright again rewind great rewind. No watch awful watch worldwide alright. Comic flawless improvement well movie.
+Movie done read. Fiction alright worldwide awful watch.', '2015-12-26');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (20, '3748596758431', 4, 'Alright greatly again no rewatch scenes. Alright flawless rewatch worldwide. Awful amazing rewind improvement scenes rewatch.', '2017-05-04');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (16, '1234567890123', 4, 'Well alright great movie fiction. Movie flawless amazing comic. Greatly read movie had great scenes.
+Read rewind worldwide comic awful done bad. Good bad alright done amazing amazing.', '2018-05-04');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (13, '1234567890123', 4, 'Scenes worldwide rewatch. Good scenes again well good.
+Read comic read tv show bad book had.
+Great alright flawless greatly movie read. Again good again done real.
+Flawless rewind well alright well. Great amazing fiction had.', '2021-10-03');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (3, '9842038473847', 2, 'Greatly worldwide worldwide worldwide. Again book bad worldwide had awful.
+Read done again great had fiction comic.
+Again good comic fiction.
+No comic amazing bad comic. Had watch watch amazing no improvement book.', '2015-08-31');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (17, '1928374858349', 5, 'Amazing really yes read read again yes. Watch awful done. Book improvement really real again.
+Well had bad. Tv Show again alright great really flawless watch.', '2018-08-07');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (16, '2738490598231', 3, 'Alright flawless had real watch scenes. Greatly amazing movie again flawless. Comic no read done had fiction read.', '2017-07-26');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (12, '1928374858349', 4, 'Real rewind really flawless worldwide book greatly. No real awful worldwide. Done flawless comic watch again.
+Read book book rewind alright. Well scenes amazing awful bad really greatly worldwide.', '2018-11-03');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (17, '3748596758431', 3, 'Awful rewind good really. Rewind bad fiction real.
+Great no tv show movie no watch. Done good yes movie. Scenes rewatch tv show awful.
+Worldwide scenes rewind rewatch. Rewind tv show comic scenes. Real awful real alright had.', '2018-05-10');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (8, '9283745689127', 4, 'Book movie no rewatch had. No yes alright greatly. Had watch real good rewind great.
+Really read greatly. Had rewatch comic fiction. Scenes awful no rewind alright scenes watch.', '2020-09-12');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (15, '1234567890123', 3, 'Fiction fiction real fiction fiction tv show. Amazing read real amazing. Scenes real had book read worldwide done.       
+Awful worldwide flawless watch rewatch. Bad great worldwide well awful movie.
+Real rewind had improvement.', '2021-08-13');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (17, '9283745689127', 2, 'Read scenes great flawless. Had rewind greatly comic rewind good.
+Real watch real. Well flawless fiction scenes alright. Amazing rewind rewatch.
+Read no watch great. Yes yes comic improvement greatly.', '2016-02-29');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (7, '9283745689127', 1, 'Read yes no improvement real. Tv Show comic really awful. Had really awful watch.
+Well rewind yes fiction again. Rewatch rewatch flawless done watch really greatly. Had awful had. No movie done good.', '2020-05-13');
+
+INSERT INTO public.rating(
+        customer_id, isbn, rating, review, review_date)
+        VALUES (20, '3748596758431', 3, 'Read had really comic amazing alright. Bad had well bad. Good good movie fiction. Improvement comic alright real done awful.
+Improvement again real comic really. Rewind real greatly scenes. Done had well flawless amazing.', '2019-02-21');
