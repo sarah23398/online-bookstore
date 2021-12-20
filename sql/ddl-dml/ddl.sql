336e6dd99ada7ec8a4ed261d5d8a0a7810270745
AdiChops <aaditya.chopra@carleton.ca>
2021-12-19 21:43:44 -0500

    ddl changes

--- sql/ddl-dml/ddl.sql
+++ sql/ddl-dml/ddl.sql
@@ -1,8 +1,8 @@
 create table customer
 	(ID         		SERIAL,
 	 name				varchar(40),
-	 email      		varchar(50),
-	 password			varchar(20),
+	 email      		varchar(50) NOT NULL UNIQUE,
+	 password			varchar(20) NOT NULL,
 	 phone				varchar(11),
 	 address			varchar(80),
 	 credit_card		varchar(20),
@@ -13,9 +13,9 @@ create table "order"
 	(ID					SERIAL,
 	 customer_id		bigint,
 	 order_date			timestamp,
-	 credit_card		varchar(20),
-	 billing_address	varchar(80),
-	 destination		varchar(80),
+	 credit_card		varchar(20) NOT NULL,
+	 billing_address	varchar(80) NOT NULL,
+	 destination		varchar(80) NOT NULL,
 	 status				varchar(10),
 	 primary key(ID),
 
@@ -53,24 +53,24 @@ create table author
 
 create table publisher
 	(ID				SERIAL,
-	 name			varchar(40),
-	 email			varchar(50),
-	 phone			varchar(11),
-	 address		varchar(80),
-	 bank_account	varchar(20),
+	 name			varchar(40) NOT NULL,
+	 email			varchar(50) NOT NULL,
+	 phone			varchar(11) NOT NULL,
+	 address		varchar(80) NOT NULL,
+	 bank_account	varchar(20) NOT NULL,
 	 primary key (ID)
 	);
 
 create table book
 	(ISBN			varchar(13),
 	 publisher_id	bigint,
-	 title			varchar(50),
-	 publish_date	date,
+	 title			varchar(50) NOT NULL,
+	 publish_date	date NOT NULL,
 	 edition		numeric(2,0),
 	 description	varchar(255),
 	 price			numeric(5,2),
 	 print_length	numeric(4,0),
-	 stock			numeric(3,0),
+	 stock			numeric(3,0) NOT NULL,
 	 publisher_fee	numeric(5,2),
 	 primary key (ISBN),
 	 foreign key (publisher_id) references publisher (ID)
@@ -112,7 +112,7 @@ create table rating
 	(ID				SERIAL,
 	 customer_id	bigint,
 	 ISBN			varchar(13),
-	 rating			numeric(1,0),
+	 rating			numeric(1,0) NOT NULL,
 	 review			varchar(255),
 	 review_date	date,
 	 primary key(ID),
